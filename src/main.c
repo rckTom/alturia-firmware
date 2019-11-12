842722384f79e9e677da5466a8c4589ccf87eaa5
Thomas Schmid <tom@lfence.de>
2019-11-12 20:47:31 +0100

    update beep pitches
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/main.c
+++ src/main.c
@@ -14,22 +14,22 @@ static void start_delay(int delay) {
 	while (delay-- > 0) {
 		if (delay >= 3) {
 			k_sleep(K_SECONDS(1));
-			beep(50,800);
+			beep(50,450);
 		}
 		else if (delay >= 2) {
 			k_sleep(K_MSEC(500));
-			beep(50,800);
+			beep(50,450);
 			k_sleep(K_MSEC(500));
-			beep(50,800);
+			beep(50,450);
 		} else {
 			k_sleep(K_MSEC(250));
-			beep(50,800);
+			beep(50,400);
 			k_sleep(K_MSEC(250));
-			beep(50,800);
+			beep(50,450);
 			k_sleep(K_MSEC(250));
-			beep(50,800);
+			beep(50,400);
 			k_sleep(K_MSEC(250));
-			beep(50,800);
+			beep(50,450);
 		}
 	}
 }
