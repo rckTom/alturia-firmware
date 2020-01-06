3803fc5c8f8ff9f0f051f4f1645be822059634d2
Thomas Schmid <tom@lfence.de>
2020-01-06 22:59:01 +0100

    WIP: kalmanfilter: update
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/kalman.h
+++ src/kalman.h
@@ -14,10 +14,18 @@ struct kalman_sys{
 	arm_matrix_instance_f32 B;
 	arm_matrix_instance_f32 C;
 	arm_matrix_instance_f32 D;
+	arm_matrix_instance_f32 G;
+	arm_matrix_instance_f32 GT;
+	arm_matrix_instance_f32 AT;
+	arm_matrix_instance_f32 CT;
+	arm_matrix_instance_f32 Q;
+	arm_matrix_instance_f32 R;
+	arm_matrix_instance_f32 K;
 	arm_matrix_instance_f32 x_pre;
 	arm_matrix_instance_f32 x_cor;
 	arm_matrix_instance_f32 P_pre;
 	arm_matrix_instance_f32 P_cor;
+	arm_matrix_instance_f32 y;
 };
 
 void kalman_sys_step(struct kalman_sys *sys, arm_matrix_instance_f32 *u);
