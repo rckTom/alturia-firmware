0cc564da2447e31ab0eca7b2b11781b5c98c0e09
Thomas Schmid <tom@lfence.de>
2020-01-07 17:06:58 +0100

    WIP: flightctrl: add flight state enum
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/flightctrl.h
+++ src/flightctrl.h
@@ -13,6 +13,19 @@
 #ifndef __ALTURIA_FLIGHTCTRL_H__
 #define __ALTURIA_FLIGHTCTRl_H__
 
+enum flight_state {
+        FLS_INIT,
+        FLS_PAD,
+        FLS_BOOST,
+        FLS_ABORT,
+        FLS_COAST_SLOW,
+        FLS_COAST_FAST,
+        FLS_DESCENT,
+        FLS_FREE_FALL,
+        FLS_LANDED
+};
 
+
+void flightctrl_init();
 
 #endif /* __ALTURIA_FLIGHTCTRL_H__ */
