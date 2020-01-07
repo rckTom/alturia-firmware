4c894f200390b14f2a116e13a2c39b17d0b468c0
Thomas Schmid <tom@lfence.de>
2020-01-07 16:33:46 +0100

    datalogger: add documentation
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/datalogger.h
+++ src/datalogger.h
@@ -25,12 +25,63 @@ struct log_data {
 	uint8_t data[];
 };
 
+/**
+ * dl_open_log() - Open a new log file
+ * @path: Path to the file
+ *
+ * Command the worker thread to open a new log file. If the file already exists,
+ * the file is overriden. The function is non blocking. If the work can not be
+ * queued, an negative value is returned.
+ *
+ * Return: error code. Negative for errors.
+ */
 int dl_open_log(const char *path);
+
+/**
+ * dl_close_log() - Close a open log file
+ *
+ * Command the worker thread to close a already open log file
+ */
 int dl_close_log();
 
+/**
+ * dl_alloc_track_data_buffer() - Allocate a log_data structure
+ * @log_data: Pointer to a pointer of the log_data struct
+ * @tid: Track id
+ * @size: Size in bytes for the data member of log_data
+ *
+ * Allocate a log_data structure from the datalogger internal memory pool. If
+ * submitted to the datalogger with dl_add_track_data() the memory is freed by
+ * the datalogger working thread. If not submitted, the memory must be released
+ * with k_free().
+ */
 int dl_alloc_track_data_buffer(struct log_data **data, uint8_t tid, size_t size);
+
+/**
+ * dl_add_track_data() - Submit log_data element to datalogger
+ * @log_data: log_data allocated with dl_alloc_track_data_buffer()
+ */
 int dl_add_track_data(struct log_data *data);
+
+/**
+ * dl_add_track_format_chunk() - Add a track format specifier to the log file
+ * @tid: Track id number
+ * @format: Python struct compatible format string
+ */
 int dl_add_track_format_chunk(uint8_t tid, const char *format);
+
+/**
+ * dl_add_track_names_chunk() - Add track names for a track with id tid
+ * @tid: Track id number
+ * @names: comma seperated list of column names
+ */
 int dl_add_track_names_chunk(uint8_t tid, const char *names);
+
+/**
+ * dl_add_file() - Add file content to log file
+ * @fid: File id number
+ * @path: Path to the file which should be included
+ */
 int dl_add_file(uint8_t fid, const char *path);
+
 #endif /* __DATALOGGER_H__ */
