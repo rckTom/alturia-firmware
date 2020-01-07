09e953676e931a37fcd811b30d0c1d14055e2377
Thomas Schmid <tom@lfence.de>
2020-01-07 14:35:57 +0100

    flightctrl: add source file templates
    Signed-off-by: Thomas Schmid <tom@lfence.de>

---
+++ src/flightctrl.h
@@ -0,0 +1,18 @@
+/*
+ * Alturia Firmware - The firmware for the alturia flight computer
+ *
+ * Copyright (c) Thomas Schmid, 2019
+ *
+ * Authors:
+ *  Thomas Schmid <tom@lfence.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 3.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef __ALTURIA_FLIGHTCTRL_H__
+#define __ALTURIA_FLIGHTCTRl_H__
+
+
+
+#endif /* __ALTURIA_FLIGHTCTRL_H__ */
