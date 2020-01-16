ad235f148893df07d4a76e1a13b36848973f17fd
Thomas Schmid <tom@lfence.de>
2020-01-17 00:29:50 +0100

    beeper.c: add missing license header
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/beeper.c
+++ src/beeper.c
@@ -1,3 +1,15 @@
+/*
+ * Alturia Firmware - The firmware for the alturia flight computer
+ *
+ * Copyright (c) Thomas Schmid, 2019
+ *
+ * Authors:
+ *  Thomas Schmid <tom@lfence.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 3.  See
+ * the COPYING file in the top-level directory.
+ */
+
 #include "beeper.h"
 #include <stdint.h>
 #include <drivers/pwm.h>
@@ -13,7 +25,6 @@ K_SEM_DEFINE(lock,1,1);
 static uint8_t vol = VOLUME;
 
 struct k_delayed_work work;
-
 
 struct beep_sequenz_data {
     struct k_delayed_work work;
