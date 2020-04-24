deab2477fbfae1fb033aa1d3773603cda15feb53
Thomas Schmid <tom@lfence.de>
2020-04-24 11:28:02 +0200

    add servos
    Signed-off-by: Thomas Schmid <tom@lfence.de>

---
+++ src/servos.c
@@ -0,0 +1,99 @@
+#include "servos.h"
+
+#include <zephyr.h>
+#include <init.h>
+#include <drivers/pwm.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(servos, CONFIG_LOG_DEFAULT_LEVEL);
+
+static const struct servo_config {
+	const char *pwm_controller;
+	int pwm_channel;
+	int period; /* period is not used */
+} servo_config[] = DT_SERVOS_SERVOS_PWMS;
+
+static struct servo_data_config {
+	/* PWM Device */
+	struct device *pwm_dev;
+
+	/* Maximum servo angle */
+	u32_t max_us;
+
+	/* Minimum servo angle */
+	u32_t min_us;
+} servo_data[ARRAY_SIZE(servo_config)];
+
+#define NUM_SERVOS ARRAY_SIZE(servo_config)
+
+int servo_set_max_us(u8_t servo, u32_t max_us)
+{
+	if (servo >= NUM_SERVOS) {
+		return -ENODEV;
+	}
+
+	servo_data[servo].max_us = max_us;
+	return 0;
+}
+
+int servo_set_min_us(u8_t servo, u32_t min_us)
+{
+	if (servo >= NUM_SERVOS) {
+		return -ENODEV;
+	}
+
+	servo_data[servo].min_us = min_us;
+	return 0;
+}
+
+int servo_set_us(int servo, u32_t us)
+{
+	const struct servo_data_config *data = servo_data + servo;
+
+	if (us > data->max_us) {
+		us =data->max_us;
+	} else if (us < data->min_us) {
+		us = data->min_us;
+	}
+
+	return pwm_pin_set_usec(data->pwm_dev,
+				servo_config[servo].pwm_channel,
+				servo_config[servo].period/1000,
+				us, 0);
+}
+
+int servo_set_angle(int servo, u8_t angle)
+{
+	if (servo >= NUM_SERVOS) {
+		return -ENODEV;
+	};
+
+	u32_t pw = servo_data[servo].min_us + (servo_data[servo].max_us -
+					       servo_data[servo].min_us) *
+					       angle / 180;
+
+	return pwm_pin_set_usec(servo_data[servo].pwm_dev,
+				servo_config[servo].pwm_channel,
+				servo_config[servo].period/1000,
+				pw, 0);
+}
+
+static int servo_init()
+{
+	for(int i = 0; i < NUM_SERVOS; i++) {
+		servo_data[i].pwm_dev = device_get_binding(
+						servo_config[i].pwm_controller);
+
+		if (servo_data[i].pwm_dev == NULL) {
+			LOG_ERR("PWM Device not found");
+			return -ENODEV;
+		}
+
+		servo_data[i].max_us = 2000;
+		servo_data[i].min_us = 1000;
+	}
+
+	return 0;
+}
+
+SYS_INIT(servo_init, APPLICATION, 0);
---
+++ src/servos.h
@@ -0,0 +1,12 @@
+#ifndef ALTURIA_SERVOS_H
+#define ALTURIA_SERVOS_H
+
+#include <zephyr.h>
+
+int servo_set_max_us(u8_t servo, u32_t max_us);
+int servo_set_min_us(u8_t servo, u32_t min_us);
+int servo_set_max_rate(int servo, u32_t max_rate);
+int servo_set_angle(int servo, u8_t angle);
+
+
+#endif
