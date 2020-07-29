2336fe6766347023fbdcae8d188f5bd74a7f8584
Thomas Schmid <tom@lfence.de>
2020-07-29 22:01:03 +0200

    kalman filter: add wrapper for missing filters
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/filter/kalman_filter.c
+++ src/filter/kalman_filter.c
@@ -1,18 +1,55 @@
 #include "kalman_filter.h"
-#include "arm_math.h"
+#include "math_ex.h"
+#include <arm_math.h>
 
+#include "altitude_kalman_impl.h"
 #include "constant_altitude_kalman_impl.h"
+#include "vertical_dynamics_kalman_impl.h"
 
-void constant_altitude_kal_cor(float xpre, float Ppre,
-			       float v_meas, float y, float *x_cor,
-			       float *Pcor)
+void constant_altitude_kal_cor(float xpre, float Ppre, float v_meas, float y,
+			       float *x_cor, float *Pcor)
 {
-	constant_altitude_kalman_impl_correct(&Ppre, v_meas, &xpre, &y,
-					      Pcor, x_cor);
+	constant_altitude_kalman_impl_correct(&Ppre, v_meas, &xpre, &y, Pcor,
+					      x_cor);
 }
 
-void constant_altitude_kal_pre(float xcor, float Pcor,
-			       float v_sys, float *xpre, float *Ppre)
+void constant_altitude_kal_pre(float xcor, float Pcor, float v_sys, float *xpre,
+			       float *Ppre)
 {
 	constant_altitude_kalman_impl_predict(&Pcor, v_sys, &xcor, Ppre, xpre);
+}
+
+void altitude_kal_pre(arm_matrix_instance_f32 *xcor,
+		      arm_matrix_instance_f32 *Pcor, float dt,
+		      float v_sys_pressure, arm_matrix_instance_f32 *xpre,
+		      arm_matrix_instance_f32 *Ppre)
+{
+	altitude_kalman_impl_predict(Pcor->pData, dt, v_sys_pressure,
+				     xcor->pData, Ppre->pData, xpre->pData);
+}
+
+void altitude_kal_cor(arm_matrix_instance_f32 *xpre,
+		      arm_matrix_instance_f32 *Ppre, float v_meas_altitude,
+		      float altitude, arm_matrix_instance_f32 *xcor,
+		      arm_matrix_instance_f32 *Pcor)
+{
+	altitude_kalman_impl_correct(Ppre->pData, v_meas_altitude, xpre->pData,
+				     &altitude, Pcor->pData, xcor->pData);
+}
+
+void vertical_dyn_kal_cor(mat *P_pre, float variance_meas_a,
+			  float variance_meas_h, mat *x_pre, mat *y, mat *P_cor,
+			  mat *x_cor)
+{
+	vertical_dynamics_kalman_impl_correct(
+	    P_pre->pData, variance_meas_a, variance_meas_h, x_pre->pData,
+	    y->pData, P_cor->pData, x_cor->pData);
+}
+
+void vertical_dyn_kal_pre(mat *P_cor, float dt, float variance_acc,
+			  float variance_h, mat *x_cor, mat *P_pre, mat *x_pre)
+{
+	vertical_dynamics_kalman_impl_predict(P_cor->pData, dt, variance_acc,
+					      variance_h, x_cor->pData,
+					      P_pre->pData, x_pre->pData);
 }
--- src/filter/kalman_filter.h
+++ src/filter/kalman_filter.h
@@ -1,11 +1,30 @@
 #ifndef ALTURIA__KALMAN_FILTER__H
 #define ALTURIA__KALMAN_FILTER__H
 
-void constant_altitude_kal_cor(float xpre, float Ppre,
-			       float v_meas, float y, float *x_cor,
-			       float *Pcor);
+#include "math_ex.h"
+#include <arm_math.h>
 
-void constant_altitude_kal_pre(float xcor, float Pcor,
-			       float v_sys, float *xpre, float *Ppre);
+void constant_altitude_kal_cor(float xpre, float Ppre, float v_meas, float y,
+			       float *x_cor, float *Pcor);
+
+void constant_altitude_kal_pre(float xcor, float Pcor, float v_sys, float *xpre,
+			       float *Ppre);
+
+void altitude_kal_pre(arm_matrix_instance_f32 *xcor,
+		      arm_matrix_instance_f32 *Pcor, float dt,
+		      float v_sys_pressure, arm_matrix_instance_f32 *xpre,
+		      arm_matrix_instance_f32 *Ppre);
+
+void altitude_kal_cor(arm_matrix_instance_f32 *xpre,
+		      arm_matrix_instance_f32 *Ppre, float v_meas_altitude,
+		      float altitude, arm_matrix_instance_f32 *xcor,
+		      arm_matrix_instance_f32 *Pcor);
+
+void vertical_dyn_kal_cor(mat *P_pre, float variance_meas_a,
+			  float variance_meas_h, mat *x_pre, mat *y, mat *P_cor,
+			  mat *x_cor);
+
+void vertical_dyn_kal_pre(mat *P_cor, float dt, float variance_acc,
+			  float variance_h, mat *x_cor, mat *P_pre, mat *x_pre);
 
 #endif /* ALTURIA__KALMAN_FILTER__H */
