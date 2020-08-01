6c59acbb79df01d23824ae57f173120f3bc8ddff
Thomas Schmid <tom@lfence.de>
2020-08-01 11:15:44 +0200

    math_ex: add accessors and setters for matrix instances
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/math_ex.h
+++ src/math_ex.h
@@ -3,6 +3,12 @@
 
 #include "arm_math.h"
 
-float norm2(arm_matrix_instance_f32 *vec);
+typedef arm_matrix_instance_f32 mat;
+
+#define mat_get(mat, row, col) (mat.pData[mat.numCols * row + col])
+#define mat_set(mat, row, col, value) (mat.pData[mat.numCols * row + col] = value)
+
+float norm2(mat *vec);
+
 
 #endif
