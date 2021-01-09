7c27bb3ca3b54b8274a8972279dfe74451d6d3ad
Thomas Schmid <tom@lfence.de>
2021-01-09 01:53:04 +0100

    boards: alturia_v1_2: flash_setup: silence compiler warnings
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- boards/arm/alturia_v1_2/flash_hold_pin_setup.c
+++ boards/arm/alturia_v1_2/flash_hold_pin_setup.c
@@ -3,22 +3,24 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
-static void init_flash_hold_pin()
+static int init_flash_hold_pin(const struct device *dev)
 {
+	(void)dev;
 	const struct device* gpio = device_get_binding(DT_GPIO_LABEL(DT_ALIAS(norflash), hold_gpios));
 	const int pin = DT_GPIO_PIN(DT_ALIAS(norflash), hold_gpios);
 	int rc = 0;
 
 	if (gpio == NULL) {
-		return;
+		return rc;
 	}
 
 	rc = gpio_pin_configure(gpio, pin, GPIO_OUTPUT);
 	if (rc != 0) {
-		return;
+		return rc;
 	}
 
-	rc = gpio_pin_set(gpio, pin, true);	
+	rc = gpio_pin_set(gpio, pin, true);
+	return 0;
 }
 
 SYS_INIT(init_flash_hold_pin, POST_KERNEL, 0);
