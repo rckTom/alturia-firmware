e7c4a4fd678082db36759af461f11f62d5a91f8f
Thomas Schmid <tom@lfence.de>
2021-08-18 22:57:43 +0200

    math_ex.c: add utility functions for matrix initilialization
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/math_ex.c
+++ src/math_ex.c
@@ -11,3 +11,22 @@ float norm2(arm_matrix_instance_f32 *vec)
 
 	return sqrtf(sum);
 }
+
+void mat_zero(mat *matrix)
+{
+	memset(matrix->pData, 0, (matrix->numCols * matrix->numRows) * sizeof(matrix->pData[0]));
+}
+
+int mat_identity(mat *matrix)
+{
+	if (matrix->numCols != matrix->numRows) {
+		return -1;
+	}
+
+	mat_zero(matrix);
+	for (int i = 0; i < matrix->numCols; i++) {
+		mat_set((*matrix), i, i, 1.0f);
+	}
+
+	return 0;
+}
--- src/math_ex.h
+++ src/math_ex.h
@@ -5,9 +5,20 @@
 
 typedef arm_matrix_instance_f32 mat;
 
+
+#define STATIC_MATRIX(name, row, col) \
+    static float32_t mat_##name##_data[row * col] = {0.0f}; \
+    static mat name =  { \
+        .pData = mat_##name##_data, \
+        .numRows = row, \
+        .numCols = col \
+    };
+
 #define mat_get(mat, row, col) (mat.pData[mat.numCols * row + col])
 #define mat_set(mat, row, col, value) (mat.pData[mat.numCols * row + col] = value)
 
+void mat_zero(mat *matrix);
+int mat_identity(mat *matrix);
 float norm2(mat *vec);
 
 
