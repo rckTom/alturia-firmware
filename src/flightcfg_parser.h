bbf10fe958a6a1aef6af97f08c8014c53ac2a9fe
Thomas Schmid <tom@lfence.de>
2021-08-18 22:56:14 +0200

    remove some old files
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- src/flightcfg_parser.h
+++
@@ -1,6 +0,0 @@
-#ifndef ALTURIA_FLIGHTCFG_PARSER_H
-#define ALTURIA_FLIGHTCFG_PARSER_H
-
-int parse();
-
-#endif
--- src/lua_execution_engine/lua_alturialib.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALTURIA__LUA_ALTURIALIB__H
-#define ALTURIA__LUA_ALTURIALIB__H
-
-#include "lua.h"
-
-int luaopen_alturialib(lua_State *L);
-
-#endif
